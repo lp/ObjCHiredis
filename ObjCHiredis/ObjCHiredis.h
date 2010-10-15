79f435ac57a31630948e73385e80dda44f9ab791
Louis-Philippe Perron <lp@spiralix.org>
2010-10-15 17:48:39 -0400

    basic project setup with hiredis and one target

---
+++ ObjCHiredis/ObjCHiredis.h
@@ -0,0 +1,15 @@
+//
+//  ObjCHiredis.h
+//  ObjCHiredis
+//
+//  Created by Louis-Philippe on 10-10-15.
+//  Copyright 2010 Modul. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ObjCHiredis : NSObject {
+
+}
+
+@end
---
+++ ObjCHiredis/ObjCHiredis.m
@@ -0,0 +1,14 @@
+//
+//  ObjCHiredis.m
+//  ObjCHiredis
+//
+//  Created by Louis-Philippe on 10-10-15.
+//  Copyright 2010 Modul. All rights reserved.
+//
+
+#import "ObjCHiredis.h"
+
+
+@implementation ObjCHiredis
+
+@end
