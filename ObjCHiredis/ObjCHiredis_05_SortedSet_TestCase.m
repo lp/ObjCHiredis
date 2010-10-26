5d87f5aeb7994a7292a58e7f434baada2fc1cf86
Louis-Philippe Perron <lp@spiralix.org>
2010-10-26 10:13:40 -0400

    ZREM tests

--- ObjCHiredis/ObjCHiredis_05_SortedSet_TestCase.m
+++ ObjCHiredis/ObjCHiredis_05_SortedSet_TestCase.m
@@ -28,6 +28,12 @@
 
 - (void)setUp {
 	redis = [ObjCHiredis redis];
+	[redis command:@"ZADD BASKET 1 PRUNE"];
+	[redis command:@"ZADD BASKET 2 TOMATO"];
+	[redis command:@"ZADD BASKET 3 ZUCHINI"];
+	[redis command:@"ZADD BAG 1 TOMATO"];
+	[redis command:@"ZADD BAG 2 POTATO"];
+	[redis command:@"ZADD BAG 3 ONION"];
 	[redis retain];
 }
 
@@ -37,9 +43,16 @@
 }
 
 - (void)test_01_ZADD {
-	id retVal = [redis command:@"ZADD BAG 2 ONION"];
+	id retVal = [redis command:@"ZADD PURSE 1 BRUSH"];
 	STAssertTrue([retVal isKindOfClass:[NSNumber class]], @"ZADD didn't return an NSNumber, got: %@", [retVal class]);
 	STAssertTrue([retVal isEqualToNumber:[NSNumber numberWithInt:1]], @"ZADD didn't return 1 on success, got: %d", [retVal integerValue]);
+}
+
+- (void)test_02_ZREM {
+	id retVal = [redis command:@"ZREM BAG ONION"];
+	STAssertTrue([retVal isKindOfClass:[NSNumber class]], @"ZREM didn't return an NSNumber, got %@", [retVal class]);
+	STAssertTrue([retVal isEqualToNumber:[NSNumber numberWithInt:1]], @"ZREM didn't return 1 on success, got %d", [retVal integerValue]);
+	STAssertTrue([[redis command:@"ZREM BAG CHERRY"] isEqualToNumber:[NSNumber numberWithInt:0]], @"ZREM didn't return 0 on failure, got %d", [[redis command:@"ZREM BAG CHERRY"] integerValue]); 
 }
 
 
